782612e0d02fa4c2ac796e802e4d1846ba4eaaf2
siddhartasr10 <siddhartasr9@live.com>
2025-04-14 11:46:01 +0200

    Añadido nivel identacion java y otros a 4

--- config.el
+++ config.el
@@ -106,6 +106,9 @@
 
 (doom/set-frame-opacity 92)
 
+(add-hook 'java-mode-hook
+          lambda() (setq c-basic-offset 4)
+ )
 ;; after! se usa con un paquete, a veces el paquete se llama *-mode a veces se llama * y ya
 (add-hook 'js-mode-hook
           (lambda() (setq js-indent-level 4)))
